f86f07e3e7f0ad9c66ef1dea68c84ad41ffcd3c2
Eric Wang <ezwang1@gmail.com>
2017-03-25 18:23:37 -0400

    implement register page

--- users/views.py
+++ users/views.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, request, redirect, flash, g
+from flask import Blueprint, request, redirect, flash, g, render_template
 
 from .models import User
 
@@ -6,12 +6,12 @@ blueprint = Blueprint("users", __name__, url_prefix="/user")
 
 @blueprint.route("/login/")
 def login():
-    if g.user:
-        return redirect("index")
+    # if g.user:
+    #     return redirect("index")
     if request.method == "POST":
         error = False
         try:
-            u = User.get(email == request.form.get("email", ""))
+            u = User.get(User.email == request.form.get("email", ""))
         except User.DoesNotExist:
             error = True
             return redirect(".login")
@@ -27,4 +27,26 @@ def login():
 
 @blueprint.route("/register/")
 def register():
-    return "Not yet implemented"
+    # if g.user:
+    #     return redirect("index")
+    if request.method == "POST":
+        email = request.form.get("email", None)
+        password = request.form.get("password", None)
+        confirm = request.form.get("confirm", None)
+        if not email or not password:
+            flash("Please fill out all of the fields!", "error")
+            return redirect(".register")
+        if password != confirm:
+            flash("Confirmation does not match password!", "error")
+            return redirect(".register")
+        try:
+            u = User.get(User.email == email)
+            flash("An account with this email already exists!", "error")
+            return redirect(".register")
+        except User.DoesNotExist:
+            pass
+        User.create(email=email, password=password)
+        flash("Your account has been created!", "success")
+        return redirect("index")
+    else:
+        return render_template("register.html")
