a38d1ef32e00579129adeaf4bed100f98b6f4791
ovkulkarni <ovkulkarni@users.noreply.github.com>
2017-03-25 16:55:02 -0400

    database stuff

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 
 from flask import Flask, render_template
+import settings
+
 app = Flask(__name__)
 
 @app.route("/")
@@ -8,4 +10,4 @@ def index():
     return render_template("index.html")
 
 if __name__ == "__main__":
-    app.run(threaded=True)
+    app.run(threaded=True, debug=settings.DEBUG)
