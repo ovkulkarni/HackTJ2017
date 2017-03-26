137e2c4701e44f875cf6397f887f7132dbb36b21
ovkulkarni <ovkulkarni@users.noreply.github.com>
2017-03-26 09:12:01 -0400

    string formatting

--- programs/views.py
+++ programs/views.py
@@ -6,6 +6,10 @@ from .models import Program, Trigger, Event, Condition, Loop, Block, Link, Condi
 
 from decorators import login_required
 
+import re
+
+matcher = re.compile(r"^.*{{ (.*) }}.*$")
+
 blueprint = Blueprint("programs", __name__, url_prefix="/program")
 
 ARGS_MAPPINGS = {
@@ -40,21 +44,36 @@ def save():
     db_blocks = []
     for block in prgrm:
         if block["type"] == "trigger":
-            t = Trigger.create(trigger_type="t", args=formatted_args(block), results=formatted_results(block))
+            t = Trigger.create(trigger_type=block["name"], args=formatted_args(block), results=formatted_results(block))
             b = Block.create(program=p, block_type="t", trigger=t)
             db_blocks.append(b)
         elif block["type"] == "event":
-            e = Event.create(args=formatted_args(block), action=block["name"])
+            e = Event.create(args=formatted_args(block, db_blocks), action=block["name"])
             b = Block.create(program=p, block_type="e", event=e)
             db_blocks.append(e)
     for i, val in enumerate(db_blocks[:-1]):
         l = Link.create(source=val, destination=db_blocks[i+1])
     return jsonify({"success": True})
 
-def formatted_args(block):
+def formatted_args(block, all_blocks=None):
     args = "["
     for key in ARGS_MAPPINGS[block["name"]]:
-        args += "('{}', '{}', '{}')".format(block["values"][key], key, ARGS_MAPPINGS[block["name"]][key])
+        val = block["values"][key]
+        match = matcher.match(val)
+        if match:
+            new_val = match.group(1)
+            block_type = new_val.split(".")[0]
+            val_block = None
+            for b in all_blocks:
+                if b.block_type == "t":
+                    if b.trigger.trigger_type == block_type:
+                        val_block = b
+                elif b.block_type == "e":
+                    if b.event.action == block_type:
+                        val_block = b
+            get_val = new_val.split(".")[1]
+            val = re.sub("{{.*}}", "{" + "id_{}[{}]".format(val_block.id, get_val)+ "}", val)
+        args += "('{}', '{}', '{}')".format(val, key, ARGS_MAPPINGS[block["name"]][key])
     args += "]"
     return args
 
